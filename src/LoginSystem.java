5c848228b5048137b2812ce06e1cdd98e7ed8cbd
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:39:47 +0100

    Create LoginSystem.java

---
+++ src/LoginSystem.java
@@ -0,0 +1,8 @@
+public class LoginSystem {
+
+    public static String authenticate(String username, String password) {
+        // TODO: Check credentials using if-else or ternary
+        // Return: "Login successful" or "Invalid credentials"
+        return "";
+    }
+}
