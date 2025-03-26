8b481a30c8550958c0d0ad793ac14bfd9d99b51d
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:41:14 +0100

    Create ResidentDashboard.java

---
+++ src/ResidentDashboard.java
@@ -0,0 +1,11 @@
+public class ResidentDashboard {
+
+    public static void showWelcome(String name) {
+        // TODO: Print a welcome message
+    }
+
+    public static int getDaysSinceLastReading(int lastDay) {
+        // TODO: Calculate number of days (Assume current day = 30)
+        return 0;
+    }
+}
