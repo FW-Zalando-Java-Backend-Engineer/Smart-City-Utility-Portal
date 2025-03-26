3479b3bd8a1b5d22c8ccd0d39fc12bafa2b09fbc
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:40:20 +0100

    Create BillingCalculator.java

---
+++ src/BillingCalculator.java
@@ -0,0 +1,7 @@
+public class BillingCalculator {
+
+    public static double calculateBill(int unitsUsed) {
+        // TODO: Use if-else and arithmetic to calculate electricity bill
+        return 0.0;
+    }
+}
