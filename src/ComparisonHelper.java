3bb7f493e69b5c15af85d32e22e9e4e372f4269c
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:40:35 +0100

    Create ComparisonHelper.java

---
+++ src/ComparisonHelper.java
@@ -0,0 +1,7 @@
+public class ComparisonHelper {
+
+    public static boolean isHigherUsage(int user1Units, int user2Units) {
+        // TODO: Use relational and logical operators to compare usage
+        return false;
+    }
+}
