f5336d7b8ec66bb80e3c4dc6f10d98313fbbf976
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:41:02 +0100

    Create SmartMeter.java

---
+++ src/SmartMeter.java
@@ -0,0 +1,10 @@
+public class SmartMeter {
+
+    public static void logUsage(String name, int units) {
+        // TODO: Log user name and usage
+    }
+
+    public static void logUsage(String name, int day, int units) {
+        // TODO: Overloaded version with day
+    }
+}
