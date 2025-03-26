3af77b3a8a18cdecf81a7c3a08c3691c8144b267
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:40:05 +0100

    Create UnitConverter.java

---
+++ src/UnitConverter.java
@@ -0,0 +1,17 @@
+public class UnitConverter {
+
+    public static String toBinary(int decimal) {
+        // TODO: Convert decimal to binary string
+        return "";
+    }
+
+    public static String toHex(int decimal) {
+        // TODO: Convert decimal to hexadecimal string
+        return "";
+    }
+
+    public static int binaryToDecimal(String binary) {
+        // TODO: Convert binary string to decimal
+        return 0;
+    }
+}
