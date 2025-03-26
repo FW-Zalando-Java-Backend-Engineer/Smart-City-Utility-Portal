46b726a2534a61ceea0f2f68f10a4726914dd760
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-26 09:40:50 +0100

    Create LogicChecker.java

---
+++ src/LogicChecker.java
@@ -0,0 +1,7 @@
+public class LogicChecker {
+
+    public static String classifyUsage(int units) {
+        // TODO: Use nested if-else and logical operators to classify usage
+        return "";
+    }
+}
